33b46de8d7cf51fd627f974a04ca5907936cbea2
sneha suhagir <127730605+Snehasuhagir@users.noreply.github.com>
2024-06-01 17:03:59 +0530

    Add files via upload
    circular ll

---
+++ insert_at_end_CLL.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+      int val;
+      node *next;
+
+      node(int data){
+        val = data;
+        next = NULL;
+      }
+};
+class circular_LL{
+  public:
+    node *head;
+     
+     circular_LL(){
+        head = NULL;
+     }
+  void display(){
+    node *temp = head;
+    do{
+        cout<<temp->val<<"->";
+        temp = temp->next;
+    }
+    while(temp!=head);
+    cout<<endl;
+  }
+  void printCircular(){
+    node *temp = head;
+    for(int i=0;i<15;i++){
+        cout<<temp->val<<"->";
+        temp = temp->next;
+    }
+    cout<<endl;
+  }
+     void insertion_at_start(int val){
+        node *new_head = new node(val);
+        if(head==NULL){
+            head = new_head;
+            new_head->next = head;
+            return;
+        }
+        node *tail = head;
+        while(tail->next!=head){
+          tail = tail->next;
+        }
+        tail->next = new_head;
+        new_head->next = head;
+        head = new_head;
+     }
+     void insertion_at_end(int val){
+        node *new_head = new node(val);
+        if(head==NULL){
+            head = new_head ;
+            new_head->next = head;
+            return;
+        }
+        node *tail = head;
+        while(tail->next!=head){
+            tail = tail->next;
+        }
+        tail->next = new_head;
+        new_head->next = head;
+     }
+};
+int main(){
+  circular_LL ll;
+  ll.insertion_at_start(3);
+  ll.display();
+  ll.insertion_at_start(2);
+  ll.display();
+  ll.insertion_at_start(1);
+  ll.display();
+  //ll.printCircular();
+  ll.insertion_at_end(4);
+  ll.display();
+  ll.insertion_at_end(5);
+  ll.display();
+}
